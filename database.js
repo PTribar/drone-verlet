d709fc6aed1c9f04c2041cbf48a67e4c130759be
PTribar <127944248+PTribar@users.noreply.github.com>
2023-08-08 15:07:22 +0900

    Update database.js

--- database.js
+++ database.js
@@ -20,7 +20,7 @@ function database_init() {
 class Database {
   constructor(database) {
     this.database = database;
-    this.ref = this.database.ref('week0');
+    this.ref = this.database.ref('week1');
   }
   
   ref(name) {
